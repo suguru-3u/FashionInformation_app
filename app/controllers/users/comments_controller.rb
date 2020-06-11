529c31ac3a9e561b34214a1ead37af76543272f3
suguru-3u <suguru.13579515@gmail.com>
2020-06-11 00:14:54 +0000

    Finish Japanese correspondence of error

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -6,10 +6,8 @@ class Users::CommentsController < ApplicationController
     @comment = post.comments.new(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      # flash[:success] = "コメントしました"
       redirect_back(fallback_location: root_path)
     else
-      # flash[:success] = "コメントできませんでした"
       redirect_back(fallback_location: root_path)
     end
   end
