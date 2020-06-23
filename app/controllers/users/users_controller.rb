ff433d8f4f23bb1fa5711b7ec484c8c71d477804
suguru-3u <suguru.13579515@gmail.com>
2020-06-23 15:10:22 +0000

    Overall refactoring

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,32 +1,31 @@
 class Users::UsersController < ApplicationController
-  before_action :user_information, only:[:show,:update]
+  before_action :my_page_infomration, only:[:show,:update]
 
   def show
-    @posts = current_user.posts.order(created_at: "DESC").page(params[:page]).per(9)
-    @current_user_favorites = current_user.favorites_post.order(created_at: "DESC").page(params[:page]).per(9)
-    @current_user_comments = current_user.comments_post.order(created_at: "DESC").page(params[:page]).per(9)
-    @solution_posts = Post.where(solution: true).order(created_at: "DESC").page(params[:page]).per(9)
+    @user = User.new
+    @solution_posts = Post.where(solution: false).recent.page(params[:page]).per(9)
   end
 
   def update
-    if @user.update(user_params)
+    if current_user.update(user_params)
       NotificationMailer.user_update(current_user).deliver_now
+      flash[:primary] = 'ユーザー情報の変更が完了しました'
       redirect_to(users_users_path)
     else
-      @posts = current_user.posts
-      @current_user_favorites = current_user.favorites_post
-      @current_user_comments = current_user.comments_post
+      flash.now[:alert] = 'ユーザー情報の変更が失敗しました'
       render(:show)
     end
   end
 
   private
   def user_params
-		params.require(:user).permit(:name,:email,:sex_status,:age)
+		params.require(:user).permit(:name,:email)
   end
 
-  def user_information
-    @user = current_user
+  def my_page_infomration
+    @posts = current_user.posts.recent.page(params[:page]).per(9)
+    @current_user_favorites = current_user.favorites_post.recent.page(params[:page]).per(9)
+    @current_user_comments = current_user.comments_post.recent.page(params[:page]).per(9)
   end
 
 end
